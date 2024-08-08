f3f9e7a96577933d7d18b3d45b92d183c26ee869
Aleksey <130298093+anstrebkov@users.noreply.github.com>
2024-08-08 21:45:44 +0300

    Update streamlit_app.py
    ru

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,19 +18,19 @@ def main():
 
     st.title("GRSbot")
 
-    # Add customization options to the sidebar
-    st.sidebar.title('Select an LLM')
+    # Добавление опций настройки на боковую панель
+    st.sidebar.title('Выберите модель LLM')
     model = st.sidebar.selectbox(
-        'Choose a model',
-        ['mixtral-8x7b-32768', 'llama2-70b-4096', "llama3-8b-8192",'gemma-7b-it']
+        'Выберите модель',
+        ['mixtral-8x7b-32768', "llama3-8b-8192",'gemma-7b-it']
     )
-    conversational_memory_length = st.sidebar.slider('Conversational memory length:', 1, 10, value = 5)
+    conversational_memory_length = st.sidebar.slider('Длина памяти для диалога:', 1, 10, значение по умолчанию = 5)
 
     memory=ConversationBufferWindowMemory(k=conversational_memory_length)
 
-    user_question = st.text_area("Задай свой вопрос:")
+    user_question = st.text_area("Задайте свой вопрос:")
 
-    # session state variable
+    # переменная состояния сессии
     if 'chat_history' not in st.session_state:
         st.session_state.chat_history=[]
     else:
@@ -38,7 +38,7 @@ def main():
             memory.save_context({'input':message['human']},{'output':message['AI']})
 
 
-    # Initialize Groq Langchain chat object and conversation
+    # Инициализация объекта ChatGroq и цепочки диалога
     groq_chat = ChatGroq(
             groq_api_key=groq_api_key, 
             model_name=model
@@ -57,3 +57,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
