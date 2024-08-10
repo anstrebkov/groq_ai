9caf7579170bc2756c1d44cf61fbe382fc673b64
Aleksey <130298093+anstrebkov@users.noreply.github.com>
2024-08-10 19:28:22 +0300

    Update streamlit_app.py
    v2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,13 @@ import streamlit as st
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
-
 from groq import Groq
 import random
-
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+import chardet
 
 load_dotenv()
 
@@ -18,12 +17,10 @@ groq_api_key = os.environ['GROQ_API_KEY']
 def main():
     st.title("GRSbot with Data Dashboard")
 
-    # Sidebar for selecting the application mode
     st.sidebar.title("Выберите режим")
     app_mode = st.sidebar.selectbox("Выберите режим работы", ["GRSbot", "Data Dashboard"])
 
     if app_mode == "GRSbot":
-        # GRSbot section
         st.subheader("GRSbot")
 
         st.sidebar.title('Выберите модель LLM')
@@ -31,7 +28,7 @@ def main():
             'Выберите модель',
             ['mixtral-8x7b-32768', 'llama2-70b-4096', "llama3-8b-8192",'gemma-7b-it']
         )
-        conversational_memory_length = st.sidebar.slider('Conversational memory length:', 1, 10, value = 5)
+        conversational_memory_length = st.sidebar.slider('Conversational memory length:', 1, 10, value=5)
 
         memory = ConversationBufferWindowMemory(k=conversational_memory_length)
 
@@ -60,13 +57,18 @@ def main():
             st.write("GRSbot:", response['response'])
 
     elif app_mode == "Data Dashboard":
-        # Data Dashboard section
         st.subheader("Simple Data Dashboard")
 
         uploaded_file = st.file_uploader("Загрузите CSV файл", type="csv")
 
         if uploaded_file is not None:
-            df = pd.read_csv(uploaded_file, encoding='utf-8')
+            # Detect encoding
+            rawdata = uploaded_file.read()
+            result = chardet.detect(rawdata)
+            encoding = result['encoding']
+            uploaded_file.seek(0)  # Reset file pointer after reading
+
+            df = pd.read_csv(uploaded_file, encoding=encoding)
 
             st.subheader("Предпросмотр данных")
             st.write(df.head())
